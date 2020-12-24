31e8aa0124ae76ad486949f4c006d9bf076d4d03
Jeevan Ingale <g1@Jeevans-MacBook-Air.local>
2020-12-24 12:14:47 -0500

    mod deploy.sh and remved l8s travis

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t g1git/multi-client:latest -t g1git/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t g1git/multi-worker:latest -t g1git/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
-docker build -t g1git/multi-server:latest -t g1git/multi-server:$SHA  -f ./server/Dockerfile ./server
+docker build -t g1hubdoc/multi-client:latest -t g1hubdoc/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t g1hubdoc/multi-worker:latest -t g1hubdoc/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+docker build -t g1hubdoc/multi-server:latest -t g1hubdoc/multi-server:$SHA  -f ./server/Dockerfile ./server
 
-docker push g1git/multi-client:latest
-docker push g1git/multi-worker:latest
-docker push g1git/multi-server:latest
+docker push g1hubdoc/multi-client:latest
+docker push g1hubdoc/multi-worker:latest
+docker push g1hubdoc/multi-server:latest
 
-docker push g1git/multi-client:$SHA
-docker push g1git/multi-worker:$SHA
-docker push g1git/multi-server:$SHA
+docker push g1hubdoc/multi-client:$SHA
+docker push g1hubdoc/multi-worker:$SHA
+docker push g1hubdoc/multi-server:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=g1git/multi-server:$SHA
-kubectl set image deployments/client-deployment client=g1git/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=g1git/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=g1hubdoc/multi-server:$SHA
+kubectl set image deployments/client-deployment client=g1hubdoc/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=g1hubdoc/multi-worker:$SHA
